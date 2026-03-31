b83fda19a02e285e029524602db4e7a54ed6bbd3
Saba Attar <sabaattar@Sabas-MBP.lan>
2026-03-31 09:23:40 +0100

    improved time out issue

--- pages/login-page.ts
+++ pages/login-page.ts
@@ -27,5 +27,6 @@ export class LoginPage {
         await this.userNameInput.fill(userName);
         await this.passwordInput.fill(password);
         await this.loginButton.click();
+        await this.page.waitForLoadState('networkidle');
     }
 }
